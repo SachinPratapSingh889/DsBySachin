bb0c7b3015a86e8c3b5dcaa8b82dac5afc6396fd
Sachin Pratap Singh <91151248+SachinPratapSingh889@users.noreply.github.com>
2024-03-04 22:53:44 +0530

    Update Success.java

--- Data_structures/src/Arrays/Success.java
+++ Data_structures/src/Arrays/Success.java
@@ -81,6 +81,22 @@ public class Success {
     		a[a.length-1]= first;
     	}
     	display(a);
+	//Optimised method
+         k=k%nums.length
+    public void rotate(int[] nums, int k) {
+       reverse(nums, 0 ,nums.length-k-1);
+       reverse(nums,nums.length-k, nums.length-1);
+       reverse(nums,0, nums.length-1);
+       
+    }
+    public static void reverse(int [] nums, int i, int j){
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
     }
     
     static void q6(int [] a) {
